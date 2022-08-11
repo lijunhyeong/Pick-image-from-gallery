a5657a62d5cb5999ce79c3b4fe40d0f8e76d8134
Lee JunHyeong <72978589+lijunhyeong@users.noreply.github.com>
2022-08-11 18:05:49 +0900

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -15,12 +15,9 @@ class MainActivity : AppCompatActivity() {
     private val pickPictureButton: Button by lazy {
         findViewById(R.id.pickPictureButton)
     }
-
     private val imageView : ImageView by lazy {
         findViewById(R.id.imageView)
     }
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,16 +33,16 @@ class MainActivity : AppCompatActivity() {
                     this,
                     android.Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
-                    // todo 권한이 잘 부여되었을 때 원하는 기능 실행
+                    // Execute the desired function when well authorized
                     navigatePhotos()
 
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
-                    // todo 권한 동의 안내 팝업 확인 후 권한 팝업 띄우는 기능
+                    // Authorization consent guidance. Ability to pop permission pop-up after pop-up check
                     showPermissionPopup()
                 }
                 else -> {
-                    // 권한 요청
+                    // Request permission
                     requestPermissions(
                         arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
                         1000
@@ -64,12 +61,12 @@ class MainActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
             1000 -> {
-                // 권한이 부여된 것
+                // an authorized thing
                 if (grantResults.isEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // 권한이 부여됐을 때 가져오는 함수
+                    // Functions that get when authorization is granted
                     navigatePhotos()
                 } else {
-                    Toast.makeText(this, "권한을 거부하였습니다.", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show()
                 }
             }
             else -> {
@@ -81,14 +78,14 @@ class MainActivity : AppCompatActivity() {
     // 사진첩 이동
     private fun navigatePhotos() {
         val intent = Intent(Intent.ACTION_GET_CONTENT)
-        intent.type = "image/*"     // 이미지 타입으로 가져온다.
-        startActivityForResult(intent, 2000)        // 콜백을 통해 결과값을 가져와야되기 때문에 코드 2000 부여
+        intent.type = "image/*"     // Import as image type.
+        startActivityForResult(intent, 2000)        // Code 2000 granted because results must be obtained through callback
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // 보낸 것이 없으면 리턴(예외처리)
+        // Return if nothing has been sent (exceptional)
         if (resultCode != Activity.RESULT_OK) {
             return
         }
@@ -99,23 +96,23 @@ class MainActivity : AppCompatActivity() {
                 if (selectedImageUri != null) {
                     imageView.setImageURI(selectedImageUri)
                 } else {
-                    Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, "Failed to get pictures.", Toast.LENGTH_LONG).show()
                 }
             }
             else -> {
-                Toast.makeText(this, "사진을 가져오지 못했습니다.", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Failed to get pictures.", Toast.LENGTH_LONG).show()
             }
         }
     }
 
     private fun showPermissionPopup() {
         AlertDialog.Builder(this)
-            .setTitle("권한이 필요합니다.")
-            .setMessage("앱에서 사진을 불러오기 위해 권한이 필요합니다.")
-            .setPositiveButton("동의하기") { _, _ ->
+            .setTitle("Permission is required.")
+            .setMessage("You need permission to retrieve pictures from the app.")
+            .setPositiveButton("agree") { _, _ ->
                 requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1000)
             }
-            .setNegativeButton("취소하기") { _, _ -> }
+            .setNegativeButton("cancel") { _, _ -> }
             .create()
             .show()
     }
